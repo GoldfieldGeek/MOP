80592edb72942b48f6d415c91457937d7b6dc1a5
RKDA <rkda@mac.com>
2020-05-24 15:06:47 -0700

    initial commit part 2

---
+++ README.md
@@ -0,0 +1,7 @@
+# MOP
+
+Math Operations Package
+An SPM demo package, coppied from the good folks at AppCoda.
+
+
+
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import MOPTests
+
+var tests = [XCTestCaseEntry]()
+tests += MOPTests.allTests()
+XCTMain(tests)
